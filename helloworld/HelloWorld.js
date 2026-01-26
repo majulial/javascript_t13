2a8ebfe625fca84af6b44e486f54ee806e5c6dd2
Júlia Lima <maria.julial2514@gmail.com>
2026-01-26 11:31:22 -0300

    Projeto Hello World

---
+++ helloworld/HelloWorld.js
@@ -0,0 +1,14 @@
+const leia = require("readline-sync");
+
+
+let mensagem = leia.question("Digite uma menssagem: ");
+//mensagem = "Batatinha!"
+
+// Let = pode atribuidar usando outras variaveis 
+
+// cConst = Não pode mudar o que ta dentro ou seja nao pode atribuir 
+
+console.log(mensagem);
+
+
+
